d96cf0dc87e42e9b6cdc6f0927939a798d02643f
cecilpert <cecile.hilpert@gmail.com>
2020-06-16 16:32:18 +0200

    proper build

--- src/components/table-crispr/table-crispr.tsx
+++ src/components/table-crispr/table-crispr.tsx
@@ -325,9 +325,6 @@ export class TableCrispr {
 
     return ([<head>
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      // @ts-ignore
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-      <link href="//cdn.bootcss.com/noUiSlider/8.5.1/nouislider.min.css" rel="stylesheet" />
     </head>,
     // ***********************************************
     // ******************* SPINNER *******************
